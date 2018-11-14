9cc35582c82ccb89e6babd073fe5c3859aec71f7
moyernwentworth <43347442+moyernwentworth@users.noreply.github.com>
2018-11-13 19:02:37 -0500

    kinda working but not wuite

--- TriviaGame/src/Player.java
+++ TriviaGame/src/Player.java
@@ -1,37 +1,185 @@
-import java.util.*;
 
-public class Player {
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
-	private int playerNum;
-	private int chosenAnswer;
-	private int points;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Player extends Application{
 	
-	public Player(int p) {
+	public int ansCho = 0;
+	public String sui = null;
+	public String butOne ="Button";// getAnswer(1);
+	public String butTwo = "Button";// getAnswer(2);
+	public String butThree ="Button"; //getAnswer(3);
+	public String butFour = "Button";//getAnswer(4);
+	public String question = sui;
+	Socket s;
+	DataInputStream din;
+	DataOutputStream dout;
+	
+	
+	
+	public static void main(String args[]) {
+		launch(args);
+        new Player(); 
+
+	}
+	
+	public Player()
+	    {
+		/*/
+		 * gets connection and sets data in and out ans data stream
+		 * was working 
+		 */
+	         try
+	         {
+	             String serverName = "10.200.20.218";
+	             s=new Socket(serverName,1242 );
+	             System.out.println(s);
+	             din= new DataInputStream(s.getInputStream());
+	             dout= new DataOutputStream(s.getOutputStream());
+	             sui = din.toString();
+	             System.out.print(sui);
+	         }
+	         catch(Exception e)
+	         {
+	             System.out.println(e);
+	         }
+	     }
+	
+	public void ClientChat() throws IOException
+	     {
+		/*/
+		 * gets the answer that the client choices and sends it down to server as in
+		 * not working 
+		 */
 		
-		playerNum=p;
-		points=0;
-	}
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+	           String s1;
+	           do
+	           {
+	        	   
+	               s1=br.readLine();
+	               dout.write(ansCho);
+	               dout.flush();
+	               System.out.println("Server Message:"+din.readUTF());
+	           }
+	           while(!s1.equals("stop"));
+	    }	
 	
-	public int answer() {
-		@SuppressWarnings("resource")
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter your answer: ");
-		return chosenAnswer=s.nextInt();
-	}
 	
-	public int getCurrentAnswer() {
-		return chosenAnswer;
-	}
+	public void start(Stage primaryStage) throws Exception {
+		/*/
+		 * This is the gui and the button should update when an answer is selected  
+		 * was working
+		 */
+		
+		primaryStage.setTitle("Trivia Game");
+		GridPane grid = new GridPane();
+		grid.setGridLinesVisible(false);
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(75);
+		grid.setVgap(20);
+		
+		Text scenetitle = new Text("Trivia!");
+		scenetitle.setFont(Font.font("Tahoma", FontWeight.SEMI_BOLD, 30));
+		grid.add(scenetitle, 0, 0, 2, 1);
+		
+		Label userName = new Label("Question:");
+		grid.add(userName, 0, 1);
+		
+		Text score = new Text("Score: ");
+		grid.add(score, 0, 2);
+		
+		Text disScore = new Text("0");
+		grid.add(disScore, 1, 2);
+		
+		Text ask = new Text(question);
+		ask.setFont(Font.font("Tahoma", FontWeight.SEMI_BOLD, 12));
+		ask.setWrappingWidth(150);
+		grid.add(ask, 1,1,1, 1);
+		
+		
+		Button queOne = new Button(butOne);
+		Button queTwo = new Button(butTwo);
+		Button queThree = new Button(butThree);
+		Button queFour = new Button(butFour);
+
+		queOne.setOnAction(e->{
+			   ansCho=1;
+	           ask.setText(question);
+	           queOne.setText(butOne);
+	           queTwo.setText(butTwo);
+	           queThree.setText(butThree);
+	           queFour.setText(butFour);
+
+		});	
+		
+		grid.add(queOne, 0, 3);
+		
+		
+		queTwo.setOnAction(e->{
+			   ansCho=2;
+			   ask.setText(question);
+	           queOne.setText(butOne);
+	           queTwo.setText(butTwo);
+	           queThree.setText(butThree);
+	           queFour.setText(butFour);
+		});		
+		grid.add(queTwo, 1, 3);
+		
+		queThree.setOnAction(e->{
+			   ansCho=3;
+			   ask.setText(question);
+	           queOne.setText(butOne);
+	           queTwo.setText(butTwo);
+	           queThree.setText(butThree);
+	           queFour.setText(butFour);
+		});			
+		grid.add(queThree, 2, 3);
 	
-	public void addPoints() {
-		points++;
+		queFour.setOnAction(e->{
+			   ansCho=4;
+			   ask.setText(question);
+	           queOne.setText(butOne);
+	           queTwo.setText(butTwo);
+	           queThree.setText(butThree);
+	           queFour.setText(butFour);
+		});			
+		grid.add(queFour, 3, 3);
+		
+		
+		try {
+			if(ansCho!=0) {
+				ClientChat();
+			  }
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		
+		Scene scene = new Scene(grid);
+		primaryStage.setResizable(false);
+		primaryStage.setScene(scene);
+		primaryStage.sizeToScene();
+		primaryStage.show();
+		
 	}
-	
-	public int getCurrentPoints() {
-		return points;
-	}
-	
-	public int getCurrentPlayer() {
-		return playerNum;
-	}
+		
 }
--- TriviaGame/src/Questions.java
+++ TriviaGame/src/Questions.java
@@ -1,34 +1,41 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.util.Scanner;
 
 public class Questions { //this is the questions class beginning
-	public final int testLen = 5;
-	private String questions[] = new String[testLen];
-	private char answers[]= new char[testLen];
+	public final static int testLen = 5;
+	private static String questions[] = new String[testLen];
+	private static char answers[]= new char[testLen];
 	private int points = 0;
-
+	private static ServerSocket serverSocket;
+	static DataInputStream din;
+	static DataOutputStream dout;
+	static Socket s;
+	String ques;
+	
 	public Questions() {
-		System.out.println("Select the subject you would like to test: "+"\r\n"+"[1] General Trivia"+"\r\n"+"[2] Something Else");
-		Scanner input = new Scanner(System.in);
-		System.out.print("Your Choice: ");
-		int choice = input.nextInt();
-		
-		setQuestions(choice);
-		
+		//feed the questions, wait for an answer, and determine whether they answered correctly or not.
 		for(int q = 0; q<questions.length;q++) {
-			String ques = getQuestion(q);
+			ques = getQuestion(q);
 			System.out.println(ques);
 			
 			System.out.print("Answer: ");
-			char userAns = input.next().charAt(0);
+			char userAns = 'a'/*the button they press*/; //this is where the user makes their choice
 			char ans = getAnswer(q);
 			
 			if(userAns == ans) {
-				System.out.println("Correct!");
+				System.out.println("Correct!");	//calculate the correct answers
 				points++;
 			}
 			else System.out.println("Not quite..");
 		}
-		System.out.println("Test Complete. You scored "+points+"/5");
+		System.out.println("Test Complete. You scored "+points+"/5"); //return their score
 	}
 	
 	public char getAnswer(int num) {
@@ -39,27 +46,97 @@ public class Questions { //this is the questions class beginning
 		return questions[num];
 	}
 	
-	private void setQuestions(int test) {
+	public static void setQuestions(int test) {
 		if(test == 1) {
-			questions[1] = "Where were the 2012 Summer Olympics Held?"+"\r\n"+
+			questions[0] = "Where were the 2012 Summer Olympics Held?"+"\r\n"+
 							"[A] Berlin"+"\r\n"+
 							"[B] Madrid"+"\r\n"+
-							"[C] London"+"\r\n"+
+							"[C] London"+"\r\n"+ //correct
 							"[D] Moscow";
-			questions[2] = "True or False: Seinfeld is the current longest-running TV show.";
-			questions[3] = "Which of the following is NOT in Boston?"+"\r\n"+
+			questions[1] = "True or False: Seinfeld is the current longest-running TV show.";
+			questions[2] = "Which of the following is NOT in Boston?"+"\r\n"+
 							"[A] Fenway Park"+"\r\n"+
-							"[B] Gillette Stadium"+"\r\n"+
+							"[B] Gillette Stadium"+"\r\n"+ //correct 
 							"[C] TD Garden"+"\r\n"+
 							"[D] None of the above";
-			questions[4] = "True or False: ";
-			questions[5] = "Which U.S. President was from the Boston area?"+"\r\n"+
+			questions[3] = "True or False: ";
+			questions[4] = "Which U.S. President was from the Boston area?"+"\r\n"+
 							"[A] Abraham Lincoln"+"\r\n"+
-							"[B] John F. Kennedy"+"\r\n"+
+							"[B] John F. Kennedy"+"\r\n"+ //correct
 							"[C] Jimmy Carter"+"\r\n"+
 							"[D] Bill Clinton";
 		
 		}
 	}
+	
+	public static void setAnswers(int num) {
+		if(num == 1) {
+			answers[0] = 2;
+			answers[1] = 1;
+			answers[3] = 1;
+			answers[4] = 1;
+		}	
+	}
+	
+	public void sendQuestion() throws IOException
+    {
+	BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+          String s1;
+          do
+          {
+       	   
+              s1=br.readLine();
+              dout.writeUTF(ques);
+              dout.flush();
+              System.out.println(din.readUTF());
+          }
+          while(!s1.equals("stop"));
+   }	
+	
+	public void sendAnswer() throws IOException
+    {
+		/*/
+		 * trying to get the question and send it to the player
+		 * not working
+		 */
+	BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+          String s1;
+          do
+          {
+       	   
+              s1=br.readLine();
+              dout.writeUTF(ques);
+              dout.flush();
+              System.out.println("Server Message:"+din.readUTF());
+          }
+          while(!s1.equals("stop"));
+   }	
+	
+	public static void main(String[] args) throws IOException {
+		//prompt user to select a test type and then read their selection
+		//System.out.println("Select the subject you would like to test: "+"\r\n"+"[1] General Trivia"+"\r\n"+"[2] Something Else");
+		Scanner input = new Scanner(System.in);
+		//System.out.print("Your Choice: ");
+		//int choice = input.nextInt();
+		
+		//set the test questions based on the user decision
+		
+		/*/
+		 * makes a connection and prints worked
+		 * does work
+		 */
+		serverSocket = new ServerSocket(1242);
+        Socket server = serverSocket.accept();
+        if(server.isConnected() == true) {
+        System.out.print("worked");
+        din= new DataInputStream(s.getInputStream());
+        dout= new DataOutputStream(s.getOutputStream());
+        
+        
+        }
+        
+		//setQuestions(choice);
+		//setAnswers(choice);
+			
+	}
 }
-
